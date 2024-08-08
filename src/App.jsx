56d62064e558b30d051a8a19f1ba0bca9ea2454e
anishsaini <anishoficial@gmail.com>
2024-08-08 19:14:18 +0530

    create navbar and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
+import Header from "./Components/Header/Header.jsx";
+import "./Components/Navbar/Navbar.jsx";
+
 function App() {
-  return <>jfdbnkenk</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
